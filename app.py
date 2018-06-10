8e877cfc53269e89436c57f63a82b00d7c36222b
spencer barriball <spencey@pm.me>
2018-06-10 23:55:19 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+from flask import Flask, render_template, request,redirect, json, session
+
+app = Flask(__name__)
+app.secret_key = 'oi23hh23h2d2323-234235'
+SESSION_TYPE = 'filesystem'
+
+app.config.from_object(__name__)
+
+
+def write_to_file(filename, data):
+    with open(filename, 'a') as file:
+        file.writelines(data)
+
+
+def check_username_is_unique(username):
+    with open('static/users.txt', 'r') as user_list:
+        usernames = user_list.readlines()
+        for name in usernames:
+            if (username.strip().lower() == name.strip().lower()):
+                return False
+        return True
+
+def get_next_question(question_number):
+    question_information = {}
+    with open('static/countries.json', 'r') as json_data:
+        questions_data = json.load(json_data)
+        for question in questions_data:
+            if question['id'] == str(question_number):
+                question_information = question
+        return question_information
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if (check_username_is_unique(request.form['username'] + "\n")):
+            write_to_file('static/users.txt', request.form['username'].strip().lower() + "\n")
+            session['username'] = request.form['username']
+            session['question_number'] = 1
+            session['score'] = 0
+            return redirect(request.form['username'])
+        else:
+            error_message = 'Please use another name, I\'ll get in a muddle with that one.'
+            return render_template('index.html', error = error_message)
+    return render_template('index.html')
+
+
+@app.route('/<username>', methods=['GET', 'POST'])
+def questions(username):
+    if request.method == 'POST':
+        session['question_number'] += 1
+        session['score'] += 1
+        question_information = get_next_question(session['question_number'])
+        return render_template('question.html', question=question_information, user=username, score=session['score'])
+    else:
+        question_information = get_next_question(session['question_number'])
+        return render_template('question.html', question=question_information, user=username, score=session['score'])
+
+
+if __name__ == '__main__':
+    app.run()
